470400bf0258d69169488787557f65bc5218c51d
Shangni <shangnihu@gmail.com>
2019-02-22 14:19:47 -0500

    Finished basic functionality

---
+++ script.js
@@ -0,0 +1,43 @@
+function playSound(e) {
+  var audio = document.querySelector(`audio[id="${e.keyCode}"]`);
+//   var key = document.querySelector(`div[id="${e.keyCode}"]`);
+
+  // audio.currentTime = 0;
+  audio.play();
+}
+
+var keys = Array.from(document.querySelectorAll(".key"));
+window.addEventListener("keydown", playSound);
+
+
+
+
+
+
+
+
+
+
+// make toggle button work
+var moon = document.getElementsByClassName("fa-moon")[0];
+var sun = document.getElementsByClassName("fa-sun")[0];
+
+moon.addEventListener("click", function() {
+  if (moon.classList.contains("far")) {
+    moon.classList.remove("far");
+    moon.classList.add("fas", "disabled");
+
+    sun.classList.remove("fas", "disabled");
+    sun.classList.add("far");
+  }
+});
+
+sun.addEventListener("click", function() {
+  if (sun.classList.contains("far")) {
+    moon.classList.remove("fas", "disabled");
+    moon.classList.add("far");
+
+    sun.classList.remove("far");
+    sun.classList.add("fas", "disabled");
+  }
+});
